2d42b4871cdff75d130442159897d1f167edaa85
yuhhhy <281423846@qq.com>
2025-04-24 22:11:41 +0800

    feat: 添加标签组件和更新首页布局
    添加了新的标签组件（Tag.vue）并在首页（Home/index.vue）中引入，用于展示不同技术栈的学习进度和成就。同时，更新了首页布局，优化了页面结构和样式。

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,8 +2,47 @@
 import { onMounted, ref } from 'vue'
 import { apiGetBlogList } from '@/api/index.js'
 import { ElMessage } from 'element-plus'
+import Tag from '@/views/Tag/index.vue'
 
 const blogList = ref([])
+
+const tagList = [
+    {
+        tagImg: new URL('@/assets/HTML5.jpg', import.meta.url).href,
+        tagName: 'HTML',
+        tagHours: 100,
+        tagProcess: 90,
+        tagNumber: 19
+    },
+    {
+        tagImg: new URL('@/assets/CSS.jpg', import.meta.url).href,
+        tagName: 'CSS',
+        tagHours: 99,
+        tagProcess: 60,
+        tagNumber: 10
+    },
+    {
+        tagImg: new URL('@/assets/js.jpg', import.meta.url).href,
+        tagName: 'JavaScript',
+        tagHours: 66,
+        tagProcess: 40,
+        tagNumber: 1
+    },
+    {
+        tagImg: new URL('@/assets/Vue.png', import.meta.url).href,
+        tagName: 'Vue',
+        tagHours: 65,
+        tagProcess: 30,
+        tagNumber: 3
+    },
+      {
+        tagImg: new URL('@/assets/webpack.jpg', import.meta.url).href,
+        tagName: 'Webpack',
+        tagHours: 2,
+        tagProcess: 5,
+        tagNumber: 2
+    },
+]
 
 onMounted(() => {
     apiGetBlogList().then((data) => {
@@ -12,221 +51,201 @@ onMounted(() => {
     }).catch((e) => {
         ElMessage.error('获取博客列表失败')
     })
-    
 })
 
 </script>
 
 <template>
     <div class="home-container">
-        <div class="home-main">
-            <RouterLink v-for="blog in blogList" :to="`/blog/${blog._id}`" class="blog">
-                <div class="blog-img"
-                    :style="{ 'background-image': `url(${blog.coverImage || '/images/20171227.jpg'})` }">
-                </div>
-                <div class="blog-intro">
-                    <div class="blog-tags">
-                        <span class="blog-tag" v-for="tag in blog.tags">{{ tag }}</span>
+        <!-- banner -->
+        <div class="home-banner">
+            该博客还不知道放点什么
+        </div>
+        <div class="home-content">
+            <!-- 博客列表 -->
+            <div class="home-main">
+                <RouterLink v-for="blog in blogList" :to="`/blog/${blog._id}`" class="blog">
+                    <div class="blog-img"
+                        :style="{ 'background-image': `url(${blog.coverImage || '/images/20171227.jpg'})` }">
                     </div>
-                    <div class="blog-title">{{ blog.title }}</div>
-                    <div class="blog-time">{{ blog.date.substring(0,10) }}</div>
-                </div>
-            </RouterLink>
-        </div>
-        <div class="home-sidebar">
-            <div class="author">
-                <div class="avatar">
-                    <img src="https://avatars.githubusercontent.com/u/148607309?v=4" alt="my avatar">
-                </div>
-                <span id="name">一曝十寒</span>
-                <span id="motto">你好！我叫一曝十寒，欢迎来到我的博客！！！</span>
+                    <div class="blog-intro">
+                        <div class="blog-tags">
+                            <span class="blog-tag" v-for="tag in blog.tags">{{ tag }}</span>
+                        </div>
+                        <div class="blog-title">{{ blog.title }}</div>
+                        <div class="blog-time">{{ blog.date.substring(0, 10) }}</div>
+                    </div>
+                </RouterLink>
             </div>
-            <div class="archive">
-                <div class="archive-tags">
-                    <a href="#">
-                        <div class="tag">CSS</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">JavaScript</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">HTML</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">Vue</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">Webpack</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">工程化</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">音乐</div>
-                    </a>
-                    <a href="#">
-                        <div class="tag">游戏</div>
-                    </a>
+            <div class="home-sidebar">
+                <!-- 个人信息 -->
+                <div class="author">
+                    <div class="avatar">
+                        <img src="https://avatars.githubusercontent.com/u/148607309?v=4" alt="my avatar">
+                    </div>
+                    <span id="name">一曝十寒</span>
+                    <span id="motto">你好！我叫一曝十寒，欢迎来到我的博客！！！</span>
                 </div>
-                <div class="archives-month">
-                    <a href="#">
-                        <div class="archive-month first">
-                            <div class="date">2025 四月</div>
-                            <div class="number">1篇</div>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div class="archive-month">
-                            <div class="date">2025 四月</div>
-                            <div class="number">1篇</div>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div class="archive-month">
-                            <div class="date">2025 六月</div>
-                            <div class="number">0篇</div>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div class="archive-month">
-                            <div class="date">2025 七月</div>
-                            <div class="number">0篇</div>
-                        </div>
-                    </a>
+                <!-- 文章归档 -->
+                <div class="archive">
+                        <a href="#">
+                            <div class="archive-month first">
+                                <div class="date">2025 四月</div>
+                                <div class="number">1篇</div>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div class="archive-month">
+                                <div class="date">2025 五月</div>
+                                <div class="number">1篇</div>
+                            </div>
+                        </a>
+                </div>
+                <!-- 我的标签 -->
+                <div class="tags">
+                        <Tag v-for="tag in tagList" :tag="tag"></Tag>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
 .home-container {
     display: flex;
+    flex-direction: column;
     margin-top: 64px;
-    padding: 40px 60px;
     min-height: calc(100vh - 75px);
-    background-color: #F5F5FA;
 
-    .home-main {
-        flex: 1;
-        height: 100%;
-        margin-right: 40px;
-        display: flex;
-        flex-wrap: wrap;
-        gap: 20px;
-
-        .blog {
-            flex: 1;
-            border-radius: 30px;
-            min-height: 300px;
-            min-width: 400px;
-            overflow: hidden;
-            background-color: #fff;
-
-            .blog-img {
-                width: 100%;
-                height: 200px;
-                background-size: cover;
-                background-position: center;
-            }
-            .blog-intro {
-                padding: 20px;
-
-                .blog-tags {
-                    font-size: 14px;
-                    color: #666;
-                    margin-bottom: 10px;
-
-                    .blog-tag {
-                        padding: 5px 10px;
-                        // margin-right: 10px;
-                        &::before {
-                            content: '# ';
-                        }
-                    }
-                }
-                .blog-title {
-                    font-size: 20px;
-                    font-weight: bold;
-                    margin-bottom: 10px;
-                }
-                .blog-time {
-                    font-size: 14px;
-                    color: #666;
-                }
-
-            }
-
-        }
+    .home-banner {
+        width: 100%;
+        height: 400px;
+        background-image: url(../../assets/homeImg.jpg);
+        background-size: cover;
+        background-position: center;
+        color: #FFF;
+        font-size: 64px;
+        font-weight: 700;
+        font-family: "Google Sans", arial, sans-serif;
     }
 
-    .home-sidebar {
+    .home-content{
+        background-color: #F5F5FA;
+        padding: 40px 60px;
         display: flex;
-        flex-direction: column;
-        width: 280px;
-        min-width: 240px;
-        height: 100%;
 
-        .author {
-            min-height: 300px;
-            margin-bottom: 40px;
-            padding: 20px;
-            border-radius: 30px;
-            background-color: #D5DEFD;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            gap: 30px;
-
-            .avatar { 
-                width: 100px;
-                height: 100px;
-                border-radius: 50%;
-                overflow: hidden;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                img {
-                    width: 100%;
-                    height: 100%;
-                }
-            }
-            #name {
-                font-weight: bold;
-            }
-            #motto {
-                text-align: center;
-                max-width: 100%;
-                word-wrap: break-word;              // 长单词断行
-            }
-        }
-
-        .archive {
-            background-color: #fdfdfd;
-            flex: 1;
-            height: 100%;
-            border-radius: 30px;
-            padding: 30px;
-
-            .archive-tags {
-                padding-bottom: 20px;
-                border-bottom: 1px solid #b5b5b5;
+        .home-main {
+                flex: 1;
+                height: 100%;
+                margin-right: 40px;
                 display: flex;
                 flex-wrap: wrap;
-
-                .tag {
-                    padding: 8px 10px;
+                gap: 20px;
+        
+                .blog {
+                    flex: 1;
                     border-radius: 10px;
-                    &:hover {
-                        background-color: #eee;
+                    min-height: 300px;
+                    min-width: 400px;
+                    overflow: hidden;
+                    background-color: #fff;
+        
+                    .blog-img {
+                        width: 100%;
+                        height: 200px;
+                        background-size: cover;
+                        background-position: center;
                     }
+        
+                    .blog-intro {
+                        padding: 20px;
+        
+                        .blog-tags {
+                            font-size: 14px;
+                            color: #666;
+                            margin-bottom: 10px;
+        
+                            .blog-tag {
+                                padding: 5px 10px;
+        
+                                // margin-right: 10px;
+                                &::before {
+                                    content: '# ';
+                                }
+                            }
+                        }
+        
+                        .blog-title {
+                            font-size: 20px;
+                            font-weight: bold;
+                            margin-bottom: 10px;
+                        }
+        
+                        .blog-time {
+                            font-size: 14px;
+                            color: #666;
+                        }
+        
+                    }
+        
                 }
             }
-
-            .archives-month {
-                overflow: hidden;
+    
+        .home-sidebar {
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+            min-width: 280px;
+            height: 100%;
+    
+            .author {
+                min-height: 300px;
+                margin-bottom: 20px;
+                padding: 20px;
                 border-radius: 10px;
+                background-color: #FFF;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                gap: 30px;
+    
+                .avatar {
+                    width: 100px;
+                    height: 100px;
+                    border-radius: 50%;
+                    overflow: hidden;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+    
+                    img {
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
+    
+                #name {
+                    font-weight: bold;
+                }
+    
+                #motto {
+                    text-align: center;
+                    max-width: 100%;
+                    word-wrap: break-word; // 长单词断行
+                }
+            }
+    
+            .archive {
+                margin-bottom: 20px;
+                border-radius: 10px;
+                background-color: #fdfdfd;
+                width: 100%;
+                flex: 1;
+                padding: 16px 20px;
+                overflow: hidden;
+                border-radius: 5px;
 
                 .archive-month {
                     cursor: pointer;
@@ -242,7 +261,7 @@ onMounted(() => {
                         position: absolute;
                         right: 20px;
                         top: 20px;
-                        font-size: 16px;
+                        font-size: 14px;
                         color: #666;
                     }
 
@@ -255,7 +274,17 @@ onMounted(() => {
                     }
                 }
             }
+
+            .tags {
+                background-color: #FFF;
+                margin-bottom: 20px;
+                border-radius: 4px;
+                overflow: hidden;
+                width: 100%;           
+            }
         }
     }
+
+
 }
 </style>
---
+++ src/views/Tag/index.vue
@@ -0,0 +1,122 @@
+<script setup>
+const props = defineProps({
+    tag: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <RouterLink to="#">
+        <div class="tag">
+            <div class="black-box"></div>
+            <img :src="tag.tagImg" class="tag-img" alt="tag-background-image">
+            <div class="tag-info">
+                <div class="tag-info-left">
+                    <div class="tag-name">{{ tag.tagName }}</div>
+                    <div class="tag-process">
+                        <div class="tag-process-hours">{{ tag.tagHours }}</div>
+                        <div class="tag-process-bar">{{ tag.tagProcess }}</div>
+                    </div>
+                </div>
+                <div class="tag-info-right">
+                    <div class="tag-achievements">{{ tag.tagNumber }}</div>
+                </div>
+            </div>
+        </div>
+    </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+.tag {
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    padding-left: 5px;
+    position: relative;
+
+    &:hover {
+        .tag-img {
+            transform: translateX(120px);
+            transition: transform 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55);
+                
+        }
+        .tag-info {
+            transform: translateX(120px);
+            opacity: 0;
+            transition: all 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55);
+        }
+        .black-box {
+            transform: translateX(120px);
+            transition: transform 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55);
+        }
+    }
+
+    .black-box {
+        position: absolute;
+        left: -120px;
+        top: 0;
+        width: 120px;
+        height: 100%;
+        background-image: url(../../assets/fire1.png);
+        background-size: cover;
+        z-index: 1;
+        transform: translateX(0);
+        transition: transform 0.4s cubic-bezier(0.68, -0.55, 0.27, 1.55);
+    }
+
+    .tag-img {
+        height: 85%;
+        width: 65px;
+        border-radius: 4px;
+        object-fit: cover;  // 保持图片比例
+    }
+
+    .tag-info {
+        flex: 1;
+        height: 100%;
+        display: flex;
+        border-bottom: 1px solid #F5F5FA;
+
+        .tag-info-left {
+            flex: 1;
+            margin-left: 10px;
+            flex-basis: 90px;
+            display: flex;
+            flex-direction: column;
+
+            .tag-name {
+                margin-top: 6px;
+                height: 1.2em;
+                font-weight: 700;
+            }
+            .tag-process {
+                flex: 1;
+                .tag-process-hours {
+                    height: 60%;
+                    display: flex;
+                    align-items: end;
+                }
+                .tag-process-bar {
+                    height: 40%;
+                }
+            }
+        }
+
+        .tag-info-right {
+            flex: 1;
+            position: relative;
+
+            .tag-achievements {
+                position: absolute;
+                right: 8px;
+                bottom: 4px;
+                font-size: 15px;
+            }
+        }
+    }
+
+}
+</style>
