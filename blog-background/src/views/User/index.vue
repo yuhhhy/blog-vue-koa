d52e153e5d16a9a6b2d52e972fe54d994b5a1d30
yuhhhy <281423846@qq.com>
2025-05-19 10:38:19 +0800

    feat: (background)添加用户删除、用户信息更改表单和提交逻辑

--- blog-background/src/views/User/index.vue
+++ blog-background/src/views/User/index.vue
@@ -1,8 +1,17 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { apiGetUserList } from '@/api/userApi.js'
+import { ref, onMounted, reactive } from 'vue'
+import { apiGetUserList, apiDeleteUser, apiUpdateUser } from '@/api/userApi.js'
+import { ElMessageBox } from 'element-plus'
 
 const tableData = ref([])
+const dialogFormVisible = ref(false)
+const form = reactive({
+  userId: '',
+  username: '',
+  password: '',
+  email: ''
+})
+const formLabelWidth = 
 
 onMounted(() => {
     apiGetUserList().then((res) => {
@@ -12,6 +21,54 @@ onMounted(() => {
     })
 })
 
+function handleDelete(index, row) {
+  ElMessageBox.confirm('确定删除吗？', '提示', {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type: 'warning'
+  }).then(
+    async () => {
+      await apiDeleteUser(row.userId)
+      tableData.value.splice(index, 1)
+      ElMessageBox.alert('删除成功', '提示', {
+        confirmButtonText: '确定',
+        type: 'success'
+      })
+    }
+  ).catch(() => {
+  })
+}
+
+function handleEdit(index, row) {
+  dialogFormVisible.value = true
+  form.userId = row.userId
+  form.username = row.username
+  form.password = row.password
+  form.email = row.email
+}
+
+function handleConfirm() {
+  if (!form.username || !form.password || !form.email) {
+    ElMessageBox.alert('请填写完整信息', '提示', {
+      confirmButtonText: '确定',
+      type: 'warning'
+    })
+    return
+  }
+  apiUpdateUser(form).then((res) => {
+    ElMessageBox.alert('修改成功', '提示', {
+      confirmButtonText: '确定',
+      type: 'success'
+    })
+    const index = tableData.value.findIndex(item => item.userId === form.userId)
+    tableData.value[index].username = form.username
+    tableData.value[index].password = form.password
+    tableData.value[index].email = form.email
+  }).catch((err) => {
+    console.error(err)
+  })
+  dialogFormVisible.value = false
+}
 
 </script>
 
@@ -41,16 +98,39 @@ onMounted(() => {
       </template>
     </el-table-column>
     <el-table-column fixed="right" label="管理">
-      <template #default>
-        <el-button type="danger" size="small">
+      <template #default="scope">
+        <el-button type="danger" size="small" @click="handleDelete(scope.$index, scope.row)">
           删除
         </el-button>
-        <el-button type="primary" size="small">
+        <el-button type="primary" size="small" @click="handleEdit(scope.$index, scope.row)">
           编辑
         </el-button>
       </template>
     </el-table-column>
   </el-table>
+
+  <!-- 更改表单 -->
+  <el-dialog v-model="dialogFormVisible" title="更改用户信息" width="500">
+    <el-form :model="form">
+      <el-form-item label="用户名" label-width="80px">
+        <el-input v-model="form.username" />
+      </el-form-item>
+      <el-form-item label="密码" label-width="80px">
+        <el-input v-model="form.password" />
+      </el-form-item>
+      <el-form-item label="邮箱" label-width="80px">
+        <el-input v-model="form.email" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="handleConfirm">
+          确认
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 <style scoped>
