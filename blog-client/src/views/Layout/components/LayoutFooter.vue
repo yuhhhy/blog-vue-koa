dd03a31a072ff406179307fc82347dddfef3ac17
yuhhhy <281423846@qq.com>
2025-05-27 14:21:51 +0800

    feat: (client) footer添加联系信息，博客页banner background-size: cover改为contain

--- blog-client/src/views/Layout/components/LayoutFooter.vue
+++ blog-client/src/views/Layout/components/LayoutFooter.vue
@@ -5,12 +5,23 @@ import { RouterLink } from 'vue-router';
 <template>
     <div class="footer-info">
         <div class="left-footer">
-                <p>© 2025 一曝十寒. All rights reserved.</p>
+            <div><p>© 2025 一曝十寒. All rights reserved. </p></div>
+            <div><p>赣ICP备2025063071号</p></div>
         </div>
         <div class="right-footer">
-            <RouterLink to="/about">关于</RouterLink>
-            <RouterLink to="/archive">归档</RouterLink>
-            <a href="#">联系</a>
+            <RouterLink to="/about" class="info-title">关于</RouterLink>
+            <RouterLink to="/archive" class="info-title">归档</RouterLink>
+            <el-popover
+                popper-class="popover"
+                content="qq 281423846"
+                placement="top"
+            >
+                <template #reference>
+                    <div class="info-title">
+                        联系
+                    </div>
+                </template>
+            </el-popover>
         </div>
     </div>
 </template>
@@ -28,9 +39,28 @@ import { RouterLink } from 'vue-router';
     z-index: 999;
     font-size: 0.875rem;
 
+
+    .left-footer {
+        display: flex;
+        gap: 20px;
+
+        p {
+            margin: 0;
+            color: var(--light-dark);
+        }
+    }
+
     .right-footer {
         display: flex;
         gap: 50px;
+
+        .info-title {
+            transition: color 0.3s ease;
+            cursor: pointer;
+            &:hover {
+                color: var(--skyblue);
+            }
+        }
     }
 
     @media (max-width: 768px) {
@@ -41,4 +71,6 @@ import { RouterLink } from 'vue-router';
     }
     
 }
+
+
 </style>
